b96d8c147883f453e7f762fefd054293dd19ad9d
AdoenLunnae <unshekdelmonton@gmail.com>
2019-03-18 17:40:41 +0100

    Basic Integration (www.p)

--- main.py
+++ main.py
@@ -25,11 +25,10 @@ print(api.VerifyCredentials())
 status = api.PostUpdate('Testing the bot #HackathonASL', media='https://i.redd.it/wqtbvzgm2sm21.jpg')
 
 
-'''
 for submission in memereddit.hot(limit=200):
     response = requests.get(submission.url)
     if response.status_code == 200:
         name = submission.url.split('/')[3]
         with open("./images/{}".format(name), 'wb') as f:
             f.write(response.content)
-'''
+
