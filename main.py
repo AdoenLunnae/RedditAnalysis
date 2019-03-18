80075496979dc89a7c3ac1752606889d26635f87
AdoenLunnae <unshekdelmonton@gmail.com>
2019-03-18 16:52:41 +0100

    Twitter Base

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # -*- encoding: utf-8 -*-
 
 import praw
-import re
+import requests
+import twitter
+import random
 
 import private
 
@@ -12,7 +14,22 @@ reddit = praw.Reddit(client_id=private.client_id,
                      user_agent=private.user_agent)
 
 
-listing = reddit.subreddits.search_by_name('memes', exact=False)
+# my_subreddits = [reddit.subreddit(x.name) for x in reddit.subreddits.search_by_name('memes') if not x.over18]
 
-for item in listing:
-    print(item)
+memereddit = reddit.subreddit('memes+')
+api = twitter.Api(private.consumer_key,
+                          private.consumer_secret,
+                          private.access_token_key,
+                          private.access_token_secret)
+print(api.VerifyCredentials())
+status = api.PostUpdate('Testing the bot #HackathonASL', media='https://i.redd.it/wqtbvzgm2sm21.jpg')
+
+
+'''
+for submission in memereddit.hot(limit=200):
+    response = requests.get(submission.url)
+    if response.status_code == 200:
+        name = submission.url.split('/')[3]
+        with open("./images/{}".format(name), 'wb') as f:
+            f.write(response.content)
+'''
